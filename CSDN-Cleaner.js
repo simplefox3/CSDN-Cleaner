a337bf3f7c0ab26ee4a41eb0f5cbde4ee5a5fa97
Exisi <2078519761@qq.com>
2021-09-25 11:05:48 +0800

    V1.2 Update

--- CSDN-Cleaner.js
+++ CSDN-Cleaner.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSDN-Cleaner|下载页面移除|百度搜索csdn结果优化
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  1.进入CSDN下载界面自动关闭 2.CSDN博客文章界面下推荐中有关csdn下载的链接清除 3.百度搜索界面清除CSDN下载和聚合内容的搜索结果 4.百度界面搜索结果/相同文章去重 5.增加界面表格获取按钮，对csdn博客中的表格进行获取重绘，复制格式不混乱
 // @author       Exisi
 // @match        https://download.csdn.net/*
@@ -13,12 +13,9 @@
 // ==/UserScript==
 
 (function () {
-
     let url = window.location.href;
 
-    if (url.match(/download.csdn/)) { //不予许浏览器打开csdn相关下载界面
-        csdnClose(); //需要使用csdn下载界面可以删除
-    }
+    if (url.match(/download.csdn/)) csdnClose(); //需要使用csdn下载界面可以删除
 
     if (url.match(/blog.csdn/)) { //清除csdn推荐内的csdn下载链接
         window.onload = function () {
@@ -32,44 +29,52 @@
                 }
             }
         }
-
         let table_node = document.getElementsByClassName("table-box");
-        if (table_node[0] != null) {
-            reRormatTable(table_node); //加入表格格式绘制按钮，用于复制到word或笔记
-        }
+        if (table_node[0] != null) reRormatTable(table_node); //加入表格格式绘制按钮，用于复制到word或笔记
     }
 
     if (url.match(/baidu.com/)) {
-        let nodeList = document.getElementsByClassName("result c-container new-pmd");
         let textList = [];
-        let model = 0;
+        let model = baiduSearchModel();
+        let nodeList = document.getElementsByClassName("result c-container new-pmd");
+
         if (nodeList != null) {
             for (let i in nodeList) {
                 const t = nodeList[i].textContent;
                 if (t != null && t.search(/(CSDN下载是一个提供学习资源)|(请访问CSDN下载)|(C币\s+立即)|(立即下载\s+低至)|(csdn已为您找到关于)|(次\s+身份认)/g) > 0) { //暴力检索
                     nodeList[i].style.display = "none"; //清除baidu搜索界面的所有csdn下载链接
                 }
-                if (t != null && t.search(/(百度文库)|(百度题库)/g) > 0) {
-                    model = 1; //搜题模式下不去重
-                }
-                if (model == 0 && nodeList[i].nodeType == 1) {
+                if (model > 0 && nodeList[i].nodeType == 1) {
                     let itemNode = nodeList[i].getElementsByClassName("c-abstract")[0];
                     if (itemNode != null) {
                         let text = itemNode.textContent;
                         let t = text.slice(text.indexOf("日") + 1).replaceAll(" ", "").replaceAll(".", "") //去除csdn文章前的日期描述
                         textList.push(t); //获取每个搜索结果的小字描述,并加入列表
-                        // console.log(t);
                     }
                 }
             }
-            if (model == 0) {
-                sameBlogRemove(nodeList, textList); //清除baidu搜索所有可能重复的结果
-            }
+            if (model > 0) sameBlogRemove(nodeList, textList); //清除baidu搜索所有可能重复的结果
         }
     }
 
 
     /*---------------------------(*･∀･)／函数分割线＼(･∀･*)---------------------------*/
+    function baiduSearchModel() {
+        let content = document.getElementsByClassName("f13 c-gap-top-xsmall se_st_footer user-avatar");
+        let model = 0;
+        if (content != null) {
+            for (let i in content) {
+                if (content[i].textContent != null) {
+                    model = content[i].textContent.indexOf("百度题库") > -1 ? 0 : 1;
+                    if (model == 0) break;
+                    model = content[i].textContent.indexOf("百度文库") > -1 ? 0 : 1;
+                    if (model == 0) break;
+                }
+            }
+            return model; //搜题0 , 默认1
+        }
+    }
+
     function sameBlogRemove(nodeList, textList) {
         for (let i in textList) {
             for (let j in textList) {
